7d1f610538f602c1fae829475896ed896506b2c5
choclate-loaf <banoffe1993@gmail.com>
2026-02-15 21:06:51 +0700

    Update login-script.js
    mengganti comment dengan format Jsdoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,25 +1,69 @@
-/*
- * Catatan:
- * Gunakan fungsi goToHome() untuk menampilkan halaman home
- * Gunakan fungsi goToLogin() untuk menampilkan halaman login
- * Gunakan fungsi showPopUp() untuk menampilkan pop up error
- */
+/** @module Login-Script */
+/**
+* Membuat variabel LoginFormElement untuk tampilan Form. 
+* constant {HTMLElement}
+*/ 
+const LoginFormElement = document.querySelector('Loginform');
+ 
 
-const loginFormElement = document.querySelector('#loginForm');
-const inputEmailElement = document.querySelector('#inputEmail');
-const inputPasswordElement = document.querySelector('#inputPassword');
+/**
+* Membuat variabel inputEmailElemetn untuk tampilan input email.
+* constant {HTMLElement}
+*/
+const LoginEmailmElement = document.querySelector('LoginEmail');
 
-const expectedEmail = 'admin@dicoding.com';
-const expectedPassword = 'superpassword';
 
+/**
+* Membuat variabel inputPasswordElemetn untuk tampilan input password.
+* constant {HTMLElement}
+*/
+const LoginPasswordmElement = document.querySelector('LoginPassword');
+
+
+/**
+* Membuat variavel expectedEmail untuk menyimpan informasi email sementara.
+* constant {string}
+*/
+const exceptedEmail = 'admin@dicoding.com';
+
+
+/**
+* Membuat variavel expectedPassword untuk menyimpan informasi password sementara.
+* constant {string}
+*/
+const exceptedPassword = 'superpassword';
+
+
+/* Comment : Menambah aksi tekan pada botton. */
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
 
-  const email = inputEmailElement.value;
-  const password = inputPasswordElement.value;
-  if (email == expectedEmail && password == expectedPassword) {
-    goToHome();
-  } else {
-    showPopUp();
-  }
+/**
+* membuat variabel email untuk menyimpan nilai email yang didapatkan saat bottom ditekan. 
+* constant {string}
+*/
+conts email = inputEmailElemant.value;
+
+/**
+* membuat variabel email untuk menyimpan nilai password yang didapatkan saat bottom ditekan. 
+* constant {string}
+*/
+conts password = inputPasswordElemant.value;
+
+
+ /* comment : memastikan bahwa nilai email dan password sesuai dengan nilai yang tersimpan. 
+ if (email == expectedEmail && Password == expectedPassword) { 
+
+ /* Comment : jika sesuai maka program akan berpindah ke halaman home. */
+ goToHome();
+
+}else {
+
+  /* Comment : namun jika tidak sesuai maka akan menampilkan informasi bahwa input salah. */ 
+  showPopUp ();
+
+
+}
 });
+
+
